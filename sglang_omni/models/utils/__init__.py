7086aa6d3f1ff16a45cb9eb086b549478b6977a0
ocss884 <ocss.lin@gmail.com>
2026-02-19 06:04:42 +0000

    lint

--- sglang_omni/models/utils/__init__.py
+++ sglang_omni/models/utils/__init__.py
@@ -1,12 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 """Shared model utilities."""
 
+from sglang_omni.models.utils.common import add_prefix, get_layer_id
 from sglang_omni.models.utils.hf import instantiate_module, load_hf_config
-from sglang_omni.models.utils.common import get_layer_id, add_prefix
 
-__all__ = [
-    "instantiate_module",
-    "load_hf_config",
-    "get_layer_id",
-    "add_prefix"
-]
+__all__ = ["instantiate_module", "load_hf_config", "get_layer_id", "add_prefix"]
