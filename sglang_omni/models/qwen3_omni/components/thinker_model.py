9dd0cd31dff96f553d1da291a1e88408ea7d67de
ratish <formula733@gmail.com>
2026-06-30 23:32:36 +0400

    Remove redundant Qwen3 Omni deepstack clone

--- sglang_omni/models/qwen3_omni/components/thinker_model.py
+++ sglang_omni/models/qwen3_omni/components/thinker_model.py
@@ -708,7 +708,7 @@ class Qwen3OmniMoeThinkerTextModel(nn.Module):
             visual_pos_masks = visual_pos_masks[..., 0]
         visual_pos_masks = visual_pos_masks.to(hidden_states.device)
         visual_embeds = visual_embeds.to(hidden_states.device, hidden_states.dtype)
-        local_this = hidden_states[visual_pos_masks, :].clone() + visual_embeds
+        local_this = hidden_states[visual_pos_masks, :] + visual_embeds
         hidden_states[visual_pos_masks, :] = local_this
         return hidden_states
 
